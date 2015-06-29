38b71739fd1c10f3781aceee3413ac03ce1576a9
Godefroid Chapelle <gotcha@bubblenet.be>
2015-06-29 18:01:15 +0200

    Back to development: 0.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='collective.updatemimetype',
-    version='0.1',
+    version='0.2.dev0',
     description="Plone addon to fix Archetypes file field mimetypes",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
